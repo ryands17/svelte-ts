48db4cfdfa84b021c155c5d1f5b407edfd0d63a5
Ryan <dsouza.ryan.11@gmail.com>
2020-07-31 23:56:16 +0530

    basic scaffolding

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render } from "@testing-library/svelte";
-import App from "./App";
+import { render, screen } from '@testing-library/svelte'
+import App from './App.svelte'
 
-test("renders learn svelte link", () => {
-  const { getByText } = render(App);
-  const linkElement = getByText(/learn svelte/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders learn svelte link', () => {
+  render(App)
+  const linkElement = screen.getByText(/learn svelte/i)
+  expect(linkElement).toBeInTheDocument()
+})
